99ee9499542c1ec16234e39f2ef47fdf7813bda7
Mikail Doumi <mdoumi@k1r3p11.42mulhouse.fr>
2022-07-23 19:42:38 +0200

    Ex06

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -5,21 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdoumi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/20 09:04:54 by mdoumi            #+#    #+#             */
-/*   Updated: 2022/07/20 12:54:00 by mdoumi           ###   ########.fr       */
+/*   Created: 2022/07/16 19:48:23 by mdoumi            #+#    #+#             */
+/*   Updated: 2022/07/23 19:31:51 by mdoumi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
-void	swap(int *a, int *b)
+#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
 {
-	int	temp;
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	swap(char **a, char **b)
+{
+	char	*temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
+
+
 }
 
-int	find_max(int *tab, int size)
+int	find_max(char *tab[], int size)
 {
 	int	i;
 	int	tmax;
@@ -28,49 +42,46 @@ int	find_max(int *tab, int size)
 	tmax = 0;
 	while (i < size)
 	{
-		if (tab[tmax] < tab[i])
+		if (ft_strcmp(tab[tmax], tab[i]) < 0)
 			tmax = i;
 		i++;
 	}
 	return (tmax);
 }
 
-void	ft_sort_int_tab(int *tab, int size)
+void	ft_sort_str_tab(char *tab[], int size)
 {
 	int	tmax;
 
 	while (size > 0)
 	{
 		tmax = find_max(tab, size);
-		swap(&tab[tmax], &tab[size - 1]);
+		swap(&tab[tmax], &tab[size-1]);
+
 		size--;
 	}
 }
 
-
-
-
-
-void	ft_putstr(char *str)
+int	main(int argc, char *argv[])
 {
 	int	i;
+	int	j;
 
+	swap(&argv[0], &argv[argc-1]);
+	argv[argc-1] = "\0";
+	argc = argc - 1;
+	ft_sort_str_tab(argv, argc);
 	i = 0;
-	while (str[i])
-		i++;
-	write(1, str, i);
-}
-
-int	main(int argc, char **argv)
-{
-	int	i;
-	char	**tab;
-
-	i = 0;
-	while (argv[i+1])
+	j = 0;
+	while (i < argc)
 	{
-		tab[i] = argv[i+1];
+		while (argv[i][j] != '\0')
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		j = 0;
 		i++;
 	}
-	return (0);
 }
